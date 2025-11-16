ad0de6864af5167ef62cb18fdd256d4d5a1834ca
Saklain <saklain.logicloop@gmail.com>
2025-11-16 14:48:42 +0530

    logos added

--- app/data/builders.js
+++ app/data/builders.js
@@ -1,19 +1,19 @@
 export const builders = [
     {
       name: "Lodha",
-      logo: "https://upload.wikimedia.org/wikipedia/en/4/41/Lodha_Group_logo.png"
+      logo: "https://www.lodhagroup.com/themes/lodhanew/images/home/logo.webp"
     },
     {
       name: "Godrej",
-      logo: "https://upload.wikimedia.org/wikipedia/commons/6/66/Godrej_Logo.svg"
+      logo: "https://www.godrejproperties.com/_next/image?url=%2Fimages%2Fheader%2Flogo.png&w=1920&q=75"
     },
     {
       name: "Brigade",
-      logo: "https://upload.wikimedia.org/wikipedia/en/1/1d/Brigade_Group_Logo.png"
+      logo: "https://d1di04ifehjy6m.cloudfront.net/media/filer_public/ef/29/ef29c53f-0d72-4f37-ae07-401d093bb1bb/logo_3.png"
     },
     {
       name: "Oberoi",
-      logo: "https://upload.wikimedia.org/wikipedia/en/d/d5/Oberoi_Realty_logo.png"
+      logo: "https://d3dy70zhjs5mi1.cloudfront.net/s3fs-public/logo_4.png"
     }
   ];
   
